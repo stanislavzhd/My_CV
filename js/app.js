8469d88d156b10a64ddcc5920835bed1f4039f67
Zhdan <Stanislav.Zhdan@sigmaukraine.com>
2020-05-25 21:56:33 +0300

    added burger to mobile version

--- js/app.js
+++ js/app.js
@@ -6,3 +6,14 @@
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+$(document).ready(function(){
+    $(".cv-nav-menu__burger").click(function(){
+       $(".cv-nav-menu__list").slideToggle(500);
+    });
+    $(window).resize(function(){
+        if ($(window).width() > 768) {
+            $(".cv-nav-menu__list").removeAttr("style")
+        };
+    });
+});
